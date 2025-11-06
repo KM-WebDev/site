3add8844cefbc91c9248a4cc65084859956efa27
Jakub Pniewski <jakub.pniewski@icloud.com>
2025-11-06 17:40:03 +0100

    add hero animations

--- components/pages/home/HomeHero.tsx
+++ components/pages/home/HomeHero.tsx
@@ -3,6 +3,7 @@ import { HeaderHeightPadding } from "@/components/base/Header.client";
 import Heading from "@/components/base/Heading";
 import Section from "@/components/base/Section";
 import Text from "@/components/base/Text";
+import AnimatedContent from "@/third-party/react-bits/AnimatedContent";
 import Image from "next/image";
 
 export default function HomeHero() {
@@ -10,32 +11,81 @@ export default function HomeHero() {
         <Section className="bg-clr-bg-light py-size-xl" padded={false}>
             <HeaderHeightPadding />
             <Section.Content className="relative flex h-screen">
-                <Heading
-                    semantic="h1"
-                    className="mx-auto max-w-3xl text-center lg:text-6xl"
+                <AnimatedContent
+                    distance={30}
+                    direction="vertical"
+                    reverse={false}
+                    duration={0.7}
+                    initialOpacity={0}
+                    animateOpacity
+                    scale={1.1}
+                    threshold={0.2}
+                    delay={0.3}
                 >
-                    Stwórz z nami nowoczesną stronę internetową
-                </Heading>
-                <Text className="mx-auto max-w-3xl text-center" muted>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Repudiandae vitae velit nobis aperiam a exercitationem ea
-                    illum est ipsum corporis? Lorem ipsum dolor, sit amet
-                    consectetur adipisicing elit. Sapiente, dolor.
-                </Text>
-                <div className="mx-auto flex gap-4 text-sm tracking-wider">
-                    <Button text="Zamów Audyt" variant="transparent" />
-                    <Button text="Zakup Stronę" />
-                </div>
-                <div className="py-size-xl bg-clr-bg px-size-xl rounded-xl">
-                    <div className="relative aspect-[4/4] w-full">
-                        <Image
-                            src={"/template-hero2.webp"}
-                            fill
-                            alt="hero"
-                            className="object-contain"
-                        />
+                    <Heading
+                        semantic="h1"
+                        className="mx-auto max-w-3xl text-center lg:text-6xl"
+                    >
+                        Stwórz z nami nowoczesną stronę internetową
+                    </Heading>
+                </AnimatedContent>
+                <AnimatedContent
+                    distance={30}
+                    direction="vertical"
+                    reverse={false}
+                    duration={0.7}
+                    initialOpacity={0}
+                    animateOpacity
+                    scale={1.1}
+                    threshold={0.2}
+                    delay={0.6}
+                >
+                    <Text className="mx-auto max-w-3xl text-center" muted>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Repudiandae vitae velit nobis aperiam a
+                        exercitationem ea illum est ipsum corporis? Lorem ipsum
+                        dolor, sit amet consectetur adipisicing elit. Sapiente,
+                        dolor.
+                    </Text>
+                </AnimatedContent>
+                <AnimatedContent
+                    distance={30}
+                    direction="vertical"
+                    reverse={false}
+                    duration={0.7}
+                    initialOpacity={0}
+                    animateOpacity
+                    scale={1.1}
+                    threshold={0.2}
+                    delay={0.9}
+                >
+                    <div className="flex justify-center gap-4 text-sm tracking-wider">
+                        <Button text="Zamów Audyt" variant="transparent" />
+                        <Button text="Zakup Stronę" />
                     </div>
-                </div>
+                </AnimatedContent>
+                <AnimatedContent
+                    distance={30}
+                    direction="vertical"
+                    reverse={false}
+                    duration={0.7}
+                    initialOpacity={0}
+                    animateOpacity
+                    scale={1.1}
+                    threshold={0.2}
+                    delay={1.2}
+                >
+                    <div className="p-size-sm bg-clr-bg-dark rounded-xl">
+                        <div className="relative aspect-[4/4] w-full">
+                            <Image
+                                src={"/template-hero2.webp"}
+                                fill
+                                alt="hero"
+                                className="object-contain"
+                            />
+                        </div>
+                    </div>
+                </AnimatedContent>
             </Section.Content>
         </Section>
     );

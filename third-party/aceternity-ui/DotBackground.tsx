07eac33292c71b6c1c5eb2b6052cbb72bedcad2e
Jakub Pniewski <jakub.pniewski@icloud.com>
2025-11-10 12:33:49 +0100

    chore(thirdparty): add new and update some old components

--- third-party/aceternity-ui/DotBackground.tsx
+++ third-party/aceternity-ui/DotBackground.tsx
@@ -6,12 +6,11 @@ export function DotBackground() {
             className={cn(
                 "absolute inset-0",
                 "[background-size:20px_20px]",
-                "[background-image:radial-gradient(#d4d4d4_1px,transparent_1px)]",
-                "dark:[background-image:radial-gradient(#404040_1px,transparent_1px)]"
+                "[background-image:radial-gradient(var(--color-clr-bg-dark)_1px,transparent_1px)]"
             )}
         >
             {/* Radial gradient for the container to give a faded look */}
-            <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
+            <div className="pointer-events-none absolute inset-0 flex items-center justify-center [mask-image:radial-gradient(ellipse_at_center,var(--color-clr-bg)_20%,var(--color-clr-bg))]"></div>
         </div>
     );
 }
